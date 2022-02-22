7077329bd306bd469ce2308b5f4208dfcfa58f58
khadetou <khadetou96@gmail.com>
2022-02-22 01:29:29 +0000

    css settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,13 @@ module.exports = {
         "gradient-bar": "linear-gradient(103.22deg, #AE67FA -13.86%, #F49867 99.55%)"
       },
       colors: {
-        "bg": "#040c18",
-        "footer": "#031B34",  
-        "text": "#81AFDD",
-        "subtext": "#FF8A71"
+        bg: "#040c18",
+        footer: "#031B34",  
+        text: "#81AFDD",
+        subtext: "#FF8A71"
+      },
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
       }
     },
   },
