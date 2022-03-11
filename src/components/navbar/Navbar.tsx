52b4ad79520c39c48d6038d3d2dff555e8c59fcb
root <root@Khadetou.localdomain>
2022-03-11 21:15:05 +0000

    header

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,11 +4,43 @@ import Image from 'next/image';
 import Logo from '/public/assets/images/Logo.svg';
 
 const Navbar: FC = () => {
+	const navbarItems = [
+		{
+			title: "Home",
+			href: "#home"
+		},
+		{
+			title: "What is GPT3?",
+			href: "#home"
+		},
+		{
+			title: "Open AI",
+			href: "#home"
+		},
+		{
+			title: "Case Studies",
+			href: "#home"
+		},
+		{
+			title: "Library",
+			href: "#home"
+		}
+	];
+
 	return (
-		<div>
-			<div>
-				<div>
-					<Image src={Logo} alt="logo" />
+		<div className="flex justify-between items-center py-8 px-24 ">
+			<div className='flex-1 flex justify-start items-center'>
+				<div className='mr-8'>
+					<Image src={Logo} width={63} height={16} alt="logo" />
+				</div>
+				<div className="flex flex-row">
+					{
+					navbarItems.map(({title, href}, key) => 
+				(	<p className="text-white font-medium text-lg leading-6 capitalize my-0 mx-4 cursor-pointer" key={key}>
+						<a href={href}>{title}</a>
+					</p>))
+					}
+
 				</div>
 			</div>
 		</div>
