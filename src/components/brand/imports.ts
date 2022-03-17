5680fa5e4f9b985aeaf3fe307150e76b0e9986f4
root <root@Khadetou.localdomain>
2022-03-17 22:39:26 +0000

    imports

---
+++ src/components/brand/imports.ts
@@ -0,0 +1,6 @@
+import google from "/public/assets/images/google.png";
+import slack from "/public/assets/images/slack.png";
+import atlassian from "/public/assets/images/atlassian.png";
+import shopify from "/public/assets/images/shopify.png";
+
+export { google, slack, atlassian, shopify };
