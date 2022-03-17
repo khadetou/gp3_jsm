a5e51907924bcd4421a21bb92e2ca2458678bc77
root <root@Khadetou.localdomain>
2022-03-17 22:51:44 +0000

    brand section

--- src/components/brand/Brand.tsx
+++ src/components/brand/Brand.tsx
@@ -1,7 +1,22 @@
-import { FC } from 'react';
+import { FC } from "react";
+import { atlassian, google, shopify, slack, dropbox } from "./imports";
+import Image from "next/image";
 
 const Brand: FC = () => {
-	return <div>Brand</div>;
+  const brands = [google, atlassian, shopify, slack, dropbox];
+
+  return (
+    <div className="flex flex-wrap justify-center items-center">
+      {brands.map((image, idx) => (
+        <div
+          key={idx}
+          className="flex-1 max-w-[150px] min-w-[120px] m-4 flex justify-center items-center"
+        >
+          <Image src={image} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Brand;
--- src/components/brand/imports.ts
+++ src/components/brand/imports.ts
@@ -2,5 +2,6 @@ import google from "/public/assets/images/google.png";
 import slack from "/public/assets/images/slack.png";
 import atlassian from "/public/assets/images/atlassian.png";
 import shopify from "/public/assets/images/shopify.png";
+import dropbox from "/public/assets/images/dropbox.png";
 
-export { google, slack, atlassian, shopify };
+export { google, slack, atlassian, shopify, dropbox };
